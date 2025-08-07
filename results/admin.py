e26eaa4288ecf60cdf74f94773879e5d088ef4cc
devangspsingh <devangshauryapratapsingh@gmail.com>
2025-08-07 20:01:27 +0530

    revert

--- results/admin.py
+++ results/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
-from django.db.models import Count, Max
-from django.contrib.auth import get_user_model
-from django.utils.html import format_html
 from .models import ResultQuery, StudentResult, ManualResultEntry
-
-User = get_user_model()
+from django.utils.html import format_html
 
 
 class UserAnonymousFilter(admin.SimpleListFilter):
@@ -28,9 +24,6 @@ class UserAnonymousFilter(admin.SimpleListFilter):
 
 @admin.register(ResultQuery)
 class ResultQueryAdmin(admin.ModelAdmin):
-    # Point to the custom template
-    change_list_template = 'admin/results/resultquery/change_list.html'
-
     list_display = [
         'roll_number_with_name', 'user_display', 'ip_address', 'timestamp',
         'result_found', 'semester', 'branch'
@@ -44,40 +37,14 @@ class ResultQueryAdmin(admin.ModelAdmin):
     readonly_fields = ['timestamp']
     date_hierarchy = 'timestamp'
 
-    def changelist_view(self, request, extra_context=None):
-        """Override to add analytics to the context."""
-        extra_context = extra_context or {}
-
-        # --- Analytics Queries ---
-        # 1. Top 15 users who searched the most
-        extra_context['top_users'] = User.objects.filter(resultquery__isnull=False).annotate(
-            query_count=Count('resultquery'),
-            last_ip=Max('resultquery__ip_address')
-        ).order_by('-query_count')[:15]
-
-        # 2. Top 15 most searched roll numbers
-        extra_context['top_roll_numbers'] = ResultQuery.objects.values('roll_number').annotate(
-            count=Count('roll_number')
-        ).order_by('-count')[:15]
-
-        # 3. Top 15 IP addresses with the most queries
-        extra_context['top_ip_addresses'] = ResultQuery.objects.values('ip_address').annotate(
-            count=Count('ip_address')
-        ).order_by('-count')[:15]
-        
-        # 4. General Stats
-        extra_context['total_queries'] = ResultQuery.objects.count()
-        extra_context['unique_roll_numbers'] = ResultQuery.objects.values('roll_number').distinct().count()
-        extra_context['unique_ip_addresses'] = ResultQuery.objects.values('ip_address').distinct().count()
-
-        return super().changelist_view(request, extra_context=extra_context)
-
     def roll_number_with_name(self, obj):
         # Try to get the name from related StudentResult or ManualResultEntry
         name = None
+        # Try StudentResult
         try:
             name = StudentResult.objects.get(roll_number=obj.roll_number).name
         except StudentResult.DoesNotExist:
+            # Try ManualResultEntry
             try:
                 name = ManualResultEntry.objects.get(roll_number=obj.roll_number).name
             except ManualResultEntry.DoesNotExist:
@@ -96,7 +63,9 @@ class ResultQueryAdmin(admin.ModelAdmin):
     user_display.short_description = "User"
 
     def get_search_results(self, request, queryset, search_term):
+        # Optionally, allow searching by student name
         queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        # Search in StudentResult and ManualResultEntry for matching names
         student_results = StudentResult.objects.filter(name__icontains=search_term)
         manual_results = ManualResultEntry.objects.filter(name__icontains=search_term)
         roll_numbers = list(student_results.values_list('roll_number', flat=True)) + \
@@ -114,7 +83,7 @@ class StudentResultAdmin(admin.ModelAdmin):
     readonly_fields = ['total_marks', 'percentage', 'created_at', 'updated_at']
     
     def get_readonly_fields(self, request, obj=None):
-        if obj: # Editing existing object
+        if obj:  # Editing existing object
             return self.readonly_fields + ['roll_number']
         return self.readonly_fields
 
