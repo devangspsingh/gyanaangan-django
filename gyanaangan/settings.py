5348e856034f11b9f2c7ea2d1aaa4b000d7e3576
devangspsingh <devangshauryapratapsingh@gmail.com>
2025-11-24 18:28:22 +0530

    fix

--- gyanaangan/settings.py
+++ gyanaangan/settings.py
@@ -15,7 +15,7 @@ import os
 from dotenv import load_dotenv
 from datetime import timedelta
 
-load_dotenv(".env.prod")
+load_dotenv(".env")
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
